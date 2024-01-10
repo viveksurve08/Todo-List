c7d4b2ec263018a4168f649fd59df8ffb3f34206
vs repo <vivek.surve08@gmail.com>
2024-01-11 02:16:50 +0530

    I completed  the todo list project

---
+++ src/App.js
@@ -0,0 +1,243 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BsCheckLg } from "react-icons/bs";
+
+function App() {
+  const [allTodos, setAllTodos] = useState([]);
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const [completedTodos, setCompletedTodos] = useState([]);
+  const [isCompletedScreen, setIsCompletedScreen] = useState(false);
+  const ballsRef = useRef([]);
+
+  const handleAddNewToDo = () => {
+    let newToDoObj = {
+      title: newTodoTitle,
+      description: newDescription,
+    };
+    // console.log (newToDoObj);
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push(newToDoObj);
+    // console.log (updatedTodoArr);
+    setAllTodos(updatedTodoArr);
+    localStorage.setItem("todolist", JSON.stringify(updatedTodoArr));
+    setNewDescription("");
+    setNewTodoTitle("");
+  };
+
+  useEffect(() => {
+    let savedTodos = JSON.parse(localStorage.getItem("todolist"));
+    let savedCompletedToDos = JSON.parse(
+      localStorage.getItem("completedTodos")
+    );
+    if (savedTodos) {
+      setAllTodos(savedTodos);
+    }
+
+    if (savedCompletedToDos) {
+      setCompletedTodos(savedCompletedToDos);
+    }
+  }, []);
+
+  const handleToDoDelete = (index) => {
+    let reducedTodos = [...allTodos];
+    reducedTodos.splice(index, 1);
+    // console.log (index);
+
+    // console.log (reducedTodos);
+    localStorage.setItem("todolist", JSON.stringify(reducedTodos));
+    setAllTodos(reducedTodos);
+  };
+
+  const handleCompletedTodoDelete = (index) => {
+    let reducedCompletedTodos = [...completedTodos];
+    reducedCompletedTodos.splice(index);
+    // console.log (reducedCompletedTodos);
+    localStorage.setItem(
+      "completedTodos",
+      JSON.stringify(reducedCompletedTodos)
+    );
+    setCompletedTodos(reducedCompletedTodos);
+  };
+
+  const handleComplete = (index) => {
+    const date = new Date();
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1;
+    var yyyy = date.getFullYear();
+    var hh = date.getHours();
+    var minutes = date.getMinutes();
+    var ss = date.getSeconds();
+    var finalDate =
+      dd + "-" + mm + "-" + yyyy + " at " + hh + ":" + minutes + ":" + ss;
+
+    let filteredTodo = {
+      ...allTodos[index],
+      completedOn: finalDate,
+    };
+
+    // console.log (filteredTodo);
+
+    let updatedCompletedList = [...completedTodos, filteredTodo];
+    console.log(updatedCompletedList);
+    setCompletedTodos(updatedCompletedList);
+    localStorage.setItem(
+      "completedTodos",
+      JSON.stringify(updatedCompletedList)
+    );
+    // console.log (index);
+
+    handleToDoDelete(index);
+  };
+
+  //background
+
+  useEffect(() => {
+    const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+    const numBalls = 80;
+    const balls = [];
+
+    for (let i = 0; i < numBalls; i++) {
+      const ball = document.createElement("div");
+      ball.classList.add("ball");
+      ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+      ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+      ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+      ball.style.transform = `scale(${Math.random()})`;
+      ball.style.width = `${Math.random()}em`;
+      ball.style.height = ball.style.width;
+
+      balls.push(ball);
+    }
+
+    ballsRef.current = balls;
+    ballsRef.current.forEach((ball, i, ra) => {
+      let to = {
+        x: Math.random() * (i % 2 === 0 ? -11 : 11),
+        y: Math.random() * 12,
+      };
+
+      let anim = ball.animate(
+        [
+          { transform: "translate(0, 0)" },
+          { transform: `translate(${to.x}rem, ${to.y}rem)` },
+        ],
+        {
+          duration: (Math.random() + 1) * 2000, // random duration
+          direction: "alternate",
+          fill: "both",
+          iterations: Infinity,
+          easing: "ease-in-out",
+        }
+      );
+    });
+
+    ballsRef.current.forEach((ball) => document.body.append(ball));
+
+    return () => {
+      // Clean up any resources if needed
+      ballsRef.current.forEach((ball) => ball.remove());
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>My Todos</h1>
+
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title:</label>
+            <input
+              type="text"
+              value={newTodoTitle}
+              onChange={(e) => setNewTodoTitle(e.target.value)}
+              placeholder="What's the title of your To Do?"
+            />
+          </div>
+          <div className="todo-input-item">
+            <label>Description:</label>
+            <input
+              type="text"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+              placeholder="What's the description of your To Do?"
+            />
+          </div>
+          <div className="todo-input-item">
+            <button
+              className="primary-btn"
+              type="button"
+              onClick={handleAddNewToDo}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+        <div className="btn-area">
+          <button
+            className={`secondaryBtn ${
+              isCompletedScreen === false && "active"
+            }`}
+            onClick={() => setIsCompletedScreen(false)}
+          >
+            ToDo
+          </button>
+          <button
+            className={`secondaryBtn ${isCompletedScreen === true && "active"}`}
+            onClick={() => setIsCompletedScreen(true)}
+          >
+            Completed
+          </button>
+        </div>
+        <div className="todo-list">
+          {isCompletedScreen === false &&
+            allTodos.map((item, index) => (
+              <div className="todo-list-item" key={index}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
+                <div>
+                  <AiOutlineDelete
+                    title="Delete?"
+                    className="icon"
+                    onClick={() => handleToDoDelete(index)}
+                  />
+                  <BsCheckLg
+                    title="Completed?"
+                    className=" check-icon"
+                    onClick={() => handleComplete(index)}
+                  />
+                </div>
+              </div>
+            ))}
+
+          {isCompletedScreen === true &&
+            completedTodos.map((item, index) => (
+              <div className="todo-list-item" key={index}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                  <p>
+                    {" "}
+                    <i>Completed at: {item.completedOn}</i>
+                  </p>
+                </div>
+                <div>
+                  <AiOutlineDelete
+                    className="icon"
+                    onClick={() => handleCompletedTodoDelete(index)}
+                  />
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+      <p>Copyright©2024 vscode</p>
+    </div>
+  );
+}
+
+export default App;
